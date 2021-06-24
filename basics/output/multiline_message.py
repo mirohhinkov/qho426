bd407b8b7cd166da1cf01171fbac1aca67117610
MiroslavHinkov <>
2021-06-24 12:13:14 +0000

    Updated to show multiline  message  addad

--- basics/output/multiline_message.py
+++ basics/output/multiline_message.py
@@ -1,3 +1,10 @@
 # Output multiline message to console 
-print('System Failure Imminent!')
-print('System reboot has been initiated...')
+# print('System Failure Imminent!')
+print("""
+System Failure Imminent!
+System reboot has been initiated...
+...rebooting sensory system
+...rebooting output motors
+...rebooting hover engine
+System online.
+""")
