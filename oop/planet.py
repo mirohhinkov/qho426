334aad7ccf0f23c43101f8b8f051dae19ffa14e4
MiroslavHinkov <>
2021-09-04 13:54:19 +0000

    Universe show_populations method

--- oop/planet.py
+++ oop/planet.py
@@ -3,32 +3,34 @@ from human import Human
 
 class Planet:
   def __init__(self):
-    self.humans = []
-    self.robots = []
+    self.inhabitants = {
+      'humans': [],
+      'robots': []
+    }
     self.name = 'Planet'
   
 
   def add_human(self, human):
-    self.humans.append(human)
+    self.inhabitants['humans'].append(human)
     return self
 
   def remove_human(self, human):
-    self.humans.remove(human)
+    self.inhabitants['humans'].remove(human)
     return self
 
   def add_robot(self, robot):
-    self.robots.append(robot)
+    self.inhabitants['robots'].append(robot)
     return self
 
   def remove_robot(self, robot):
-    self.robots.remove(robot)
+    self.inhabitants['robots'].remove(robot)
     return self
 
   def __repr__(self):
-    return f'Planet (name = {self.name}, humans({[repr(h) for h in self.humans]}), robots({[repr(r) for r in self.robots]})'
+    return f'Planet (name = {self.name}, humans({[repr(h) for h in self.inhabitants["humans"]]}), robots({[repr(r) for r in self.inhabitants["robots"]]})'
 
   def __str__(self):
-    return f'Planet: {self.name}\n\t{[repr(h) for h in self.humans]}\n\t{[repr(r) for r in self.robots]}'
+    return f'Planet: {self.name}\n\t{[repr(h) for h in self.inhabitants["humans"]]}\n\t{[repr(r) for r in self.inhabitants["robots"]]}'
   
 if __name__ == '__main__':
   h1 = Human()
@@ -42,5 +44,6 @@ if __name__ == '__main__':
   planet = Planet()
   planet.name = 'Earth'
   planet.add_human(h1).add_human(h2).add_robot(r1).add_robot(r2)
+  print(planet.inha)
   print(repr(planet))
   print(planet)
---
+++ oop/universe.py
@@ -0,0 +1,55 @@
+from robot import Robot
+from human import Human
+from planet import Planet
+
+from random import randint
+import matplotlib.pyplot as plt
+
+
+class Universe:
+  def __init__(self):
+    self.planets = []
+
+  def generate(self):
+    planet = Planet()
+    for i in range(randint(0, 11)):
+      planet.add_human(Human())
+    for i in range(randint(0, 11)):
+      planet.add_robot(Robot())
+    self.planets.append(planet)
+
+  def show_populations(self):
+    x = []
+    y = []
+    # Generates x and y lists
+    for i, planet in enumerate(self.planets):
+      x.extend([f"H{i+1}", f"R{i+1}", i * ' '])
+      y.extend([len(planet.inhabitants['humans']), len(planet.inhabitants['robots']), 0])
+
+    #Labels
+    plt.title("Universe planets")
+    plt.xlabel("Humans and Robots for each planets")
+    plt.ylabel("Number of inhabitants")
+
+    barlist = plt.bar(x, y)
+    # set red color for each human bar (defauls blue for robot bars)
+    for i in range(len(self.planets)):
+      barlist[3*i].set_color('r')
+    plt.savefig('./bar.png') # for replit.com 
+    # plt.show()
+    # input("")
+    # plt.close(barlist)
+
+  def __str__(self):
+    res = [repr(planet) for planet in self.planets]
+    return '\n'.join(res)
+
+if __name__ == '__main__':
+  uni = Universe()
+  uni.generate()
+  uni.generate()
+  uni.generate()
+  uni.generate()
+  uni.generate()
+  uni.generate()
+  uni.show_populations()
